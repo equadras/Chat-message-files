57cf29adb6123c3ba25a2aa820d5bae3e7bd83dc
EmanuelQuadras <emanuel.quadras@totvs.com.br>
2024-09-20 20:55:23 -0300

    ixi

--- src/Client.java
+++ src/Client.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Client {
     public static void main(String[] args) throws IOException {
-        Socket socket = new Socket("10.20.180.205", 12345);
+        Socket socket = new Socket("127.0.0.1", 12345);
         System.out.println("Cliente conectado ao servidor!");
 
         // Exibir os comandos
